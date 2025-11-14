e3ae80ff8ee0023f2fb10314a232f5f4cc04a998
lukechodgson-a11y <lukechodgson@gmail.com>
2025-11-14 15:32:35 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,28 @@
-// service worker
+
+const CACHE = 'ironbridge-cache-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e=>{
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
+      let clone = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
